23c9655f3cbe233e7352a08ed5cbdbe3a77780fd
Kye Buffery <kyebuffery14@gmail.com>
2018-10-21 16:49:04 +0100

    fixed auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,11 +55,6 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-            'remember_me' => 'boolean'
-        ]);
         $credentials = request(['email', 'password']);
         if(!Auth::attempt($credentials))
             return response()->json([
